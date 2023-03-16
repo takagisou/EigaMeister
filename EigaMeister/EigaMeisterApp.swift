77aa69ea4b89bd786f016d35cde5c9fb79341221
sana <takagisou@users.noreply.github.com>
2023-03-16 20:45:25 +0900

    Initial Commit

---
+++ EigaMeister/EigaMeisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  EigaMeisterApp.swift
+//  EigaMeister
+//
+//  Created by sana on 2023/03/16.
+//
+
+import SwiftUI
+
+@main
+struct EigaMeisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
